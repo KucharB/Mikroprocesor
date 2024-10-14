b2a13d2ece519c65755dc1f4e6b49d1af3e9bbc2
KucharekB <166820658+KucharB@users.noreply.github.com>
2024-10-14 20:45:46 +0200

    Create pc.v

---
+++ pc.v
@@ -0,0 +1,18 @@
+module pc
+( 
+    input clk,
+    input clr,
+    output reg [4:0] data
+)
+
+always @(posedge clk)
+    if (clr)
+    begin
+        data <= 0;
+    end
+    else
+    begin
+        data <= data + 1;
+    end
+
+endmodule
